02c53dc338f37c32ce15816abaa54e74b1efb828
MailaFAP <maila_3@hotmail.com>
2024-08-30 15:18:56 -0300

    coloquei os decorators do swagger no controller e na DTO para documentação, corrigi o nome DTO que estava com dois t

--- src/application/dto/enterprise.create-dto.ts
+++ src/application/dto/enterprise.create-dto.ts
@@ -1,23 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 import { TypeEnterprise } from "src/domain/enums/typeEnterprise.enum"; 
 
-export class CreateEnterpriseDTto{
-    @IsNotEmpty()
+export class CreateEnterpriseDto{
+    @ApiProperty({example: '01023524125635', description: 'CNPJ da empresa'})
     @IsNumber()
     CNPJ: number;
 
+    @ApiProperty({example: 'Nicole e Francisca Alimentos Ltda', description: 'Nome da empresa'})
     @IsNotEmpty()
     @IsString()
     companyName: string;
 
+    @ApiProperty({example: '052412635', description: 'Cep do endereço da empresa'})
     @IsNotEmpty()
     @IsNumber()
     CEP: number;
 
+    @ApiProperty({example: 'Rua da Paz, 36', description: 'Endereço da empresa'})
     @IsNotEmpty()
     @IsString()
     address: string;
 
+    @ApiProperty({example: 'manufactoring', description: 'Tipo de serviço'})
     @IsNotEmpty()
     @IsString()
     type:TypeEnterprise;
