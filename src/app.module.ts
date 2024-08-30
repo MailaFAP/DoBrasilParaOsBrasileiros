6136f6979429b7f604404e0a4dc79b84fd6ebe1b
MailaFAP <maila_3@hotmail.com>
2024-08-30 10:59:22 -0300

    config: instalei postgres e coloquei suas configurações no arquivo app.module #7

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Enterprise } from './domain/entities/enterprise.entity';
+import { ApplicationModule } from './application/application.module';
+import { DomainModule } from './domain/domain.module';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgres://default:fuEOoXeJ5T9P@ep-red-night-a46a6agi-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require',
+      entities: [Enterprise],
+      synchronize: true,
+    }),
+    ApplicationModule,
+    DomainModule,
+    InfrastructureModule
+  ],
+  controllers: [],
+  providers: [],
 })
+
 export class AppModule {}
