97d4fc6f71353b2a508828af0dbdb55a54076c31
MailaFAP <maila_3@hotmail.com>
2024-08-31 14:12:32 -0300

    exclui nomes de classes que não estão mais em uso

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnterpriseRepository } from './repositories/enterprise.respository';
-import { AgriculturalCompany, Enterprise, ManufacturingCompany } from 'src/domain/entities/enterprise.entity';
+import { Enterprise } from 'src/domain/entities/enterprise.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Enterprise])],
